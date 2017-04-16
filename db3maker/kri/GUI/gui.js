1ab4341f4362a94bdbdf82b32da744edb6746865
Mikhail Kashevich <kashee@list.ru>
2017-04-16 19:37:31 +0300

    fix5

--- db3maker/kri/GUI/gui.js
+++ db3maker/kri/GUI/gui.js
@@ -162,15 +162,21 @@ dbFileElm.onchange = function() {
 
 // Load a txt-lib from a file
 txtFileElm.onchange = function() {
-	console.log("--lol--");
+	console.log("--lol1--");
 	var f = txtFileElm.files[0];
-	var r = new FileReader();
+	var r = new FileReader();	
+	console.log("--lol2--");
 	r.onload = function() {
+	console.log("--lol3--");
 		var text = r.result;
+		console.log("--lol4--");
+
 		commandsElm2.innerHTML = text;
-		console.log(r.result.substring(0, 200));
+		console.log("--78"+r.result.substring(0, 200));
 		r.readAsText(f);
 	}
+	
+	console.log("--88"+r.result.substring(0, 200));
 	//r.readAsArrayBuffer(f);
 }
 /*      var openFile = function(event) {
